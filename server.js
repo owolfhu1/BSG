41629c7ad563caad3131277fc21db7b52be23d7b
Orion Wolf_Hubbard <owolfhu1@gmail.com>
2017-08-13 09:42:43 -0400

    added CharacterTypeEnum and added to CharacterMap

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const MAX_RESOURCE=15;
 
-//characters objects
-
 const SkillTypeEnum = Object.freeze({
     ENGINEERING:"Engineering",
     LEADERSHIP:"Leadership",
@@ -10,66 +8,290 @@ const SkillTypeEnum = Object.freeze({
     TACTICS:"Tactics",
 });
 
+const CharacterTypeEnum = Object.freeze({
+    MILITARY_LEADER:"Military Leader",
+    POLITICAL_LEADER:"Political Leader",
+    PILOT:"Pilot",
+	SUPPORT:"Support",
+});
+
 const GamePhaseEnum = Object.freeze({
     SETUP:"Setup",
     PICK_CHARACTERS:"Pick Characters",
 });
 
-const CharacterMap = Object.freeze({ //set up later for character objects
+const LocationEnum = Object.freeze({
+    
+    //Colonial One
+    PRESS_ROOM:"Press Room",
+    PRESIDENTS_OFFICE:"President's Office",
+    ADMINISTRATION:"Administration",
+    
+    //Cylon Locations
+    CAPRICA:"Caprica",
+    CYLON_FLEET:"Cylon Fleet",
+    HUMAN_FLEET:"Human Fleet",
+    RESURRECTION_SHIP:"Resurrection Ship",
+    
+    //Galactica
+    FTL_CONTROL:"FTL Control",
+    WEAPONS_CONTROL:"Weapons Control",
+    COMMUNICATIONS:"Communications",
+    RESEARCH_LAB:"Research Lab",
+    COMMAND:"Command",
+    ADMIRALS_QUARTERS:"Admiral's Quarters",
+    HANGAR_DECK:"Hangar Deck",
+    ARMORY:"ARMORY",
+    SICKBAY:"SICKBAY",
+    BRIG:"BRIG",
+    
+});
+
+const CharacterMap = Object.freeze({
 	LADAMA: {
 		name:"Lee Adama",
-		type:SkillTypeEnum.PILOTING,
+		type:CharacterTypeEnum.PILOT,
 		skills:{
 			tactics:1,
 			piloting:2,
 			leadershipPolitics:2,
 		},
+		start:'eric! i dont know what to do here -orion', //launch and pilot a viper
+		/*
+		alert viper pilot:
+			when a viper is placed in a space area from the "Reserves," you may choose
+			to pilot it and take 1 action. You may only do this when you are on
+			Galactica location. excluding the "Brig."
+		
+		CAG - Action:
+			Once per game, you may activate up to 6 unmanned vipers.
+		
+		Headstrong:
+			When you are forced to discard Skill Cards, you must discard randomly.
+		*/
+		
 	},
     BADAMA: {
-        name:"Bill Adama",
-        type:SkillTypeEnum.LEADERSHIP,
+        name:"William Adama",
+        type:CharacterTypeEnum.MILITARY_LEADER,
         skills:{
             leadership:3,
             tactics:2,
         },
+		start:LocationEnum.ADMIRALS_QUARTERS,
+		/*
+		Inspirational Leader:
+			When you draw a Crisis Card, all 1 strength Skill Cards count positive for
+		 	the skill check
+	 
+		Command Authority:
+		 	Once per game, after resolving a skill check, instead of discarding the used
+		 	Skill Cards, draw them into your hand.
+	 
+		Emotionally Attached:
+		 	You may not activate the "Admiral's Quarters" location.
+		*/
     },
-	/*
-	BALTAR:"Gaius Baltar",
-	TYROL:"Galen Tyrol",
-	THRACE:"Kara Thrace",
-	AGATHON:"Karl Agathon",
-	ROSLIN:"Laura Roslin",
-	VALERII:"Sharon Valerii",
-	TIGH:"Saul Tigh",
-	ZAREK:"Tom Zarek",
-	*/
-});
-
-const LocationEnum = Object.freeze({
-	
-	//Colonial One	
-	PRESS_ROOM:"Press Room",
-	PRESIDENTS_OFFICE:"President's Office",
-	ADMINISTRATION:"Administration",
-	
-	//Cylon Locations
-	CAPRICA:"Caprica",
-	CYLON_FLEET:"Cylon Fleet",
-	HUMAN_FLEET:"Human Fleet",
-	RESURRECTION_SHIP:"Resurrection Ship",
-		
-	//Galactica
-	FTL_CONTROL:"FTL Control",
-	WEAPONS_CONTROL:"Weapons Control",
-	COMMUNICATIONS:"Communications",
-	RESEARCH_LAB:"Research Lab",
-	COMMAND:"Command",
-	ADMIRALS_QUARTERS:"Admiral's Quarters",
-	HANGAR_DECK:"Hangar Deck",
-	ARMORY:"ARMORY",
-	SICKBAY:"SICKBAY",
-	BRIG:"BRIG",
-	
+	BALTAR:{
+        name:"Gaius Baltar",
+        type:CharacterTypeEnum.POLITICAL_LEADER,
+        skills:{
+            politics:2,
+            leadership:1,
+            engineering:1,
+        },
+		start:LocationEnum.RESEARCH_LAB,
+		/*
+		Delusional Intuition:
+			After you draw a Crisis Card, draw 1 Skill Card of your choice (it may be
+		 	from outside your skill set).
+		 
+		Cylon Detector - Action:
+			Once per game, you may look at all Loyalty Cards belonging to another player.
+		 
+		Coward:
+			You start the game with 2 loyalty Cards (instead of 1).
+		*/
+    },
+	TYROL:{
+        name:'"Chief" Galen Tyrol',
+        type:CharacterTypeEnum.SUPPORT,
+        skills:{
+            politics:1,
+            leadership:2,
+            engineering:2,
+        },
+		/*
+		Maintenance Engineer:
+			During your turn, after you use a "Repair" Skill Card, you may take another
+			action (once per tern).
+		 
+		Blind Devotion:
+			Once per game, after cards have been added to a skill Check (but before
+			revealing them), you may choose a skill type. All cards of the chosen type
+			are considered strength 0.
+		 
+		Reckless:
+			Your hand limit is 8 (instead of 10).
+		*/
+    },
+	THRACE:{
+        name:'Kara "Starbuck" Thrace',
+        type:CharacterTypeEnum.PILOT,
+        skills:{
+            tactics:2,
+            piloting:2,
+            leadershipengineering:1,
+        },
+		start: LocationEnum.HANGAR_DECK,
+		/*
+		Expert Pilot:
+			When you start your turn piloting a viper, you may take 2 actions during
+			your Action Step (instead of 1).
+		 
+		Secret Destiny:
+			Once per game, immediately after a Crisis Card is revealed, discard it
+			and draw a new one.
+		 
+		Insubordinate:
+			When a player chooses you with the "Admiral's Quarters" location,
+			reduce the difficulty by 3.
+		*/
+    },/* -i think this one is an expansion
+	AGATHON:{
+        name:'Karl "Helo" Agathon',
+        type:CharacterTypeEnum.PILOT,
+        skills:{
+            leadership:2,
+            tactics:2,
+            piloting:1,
+        },
+        start: LocationEnum.ADMIRALS_QUARTERS,
+		/*
+		Raptor Pilot:
+			When you look at the top card of a deck as a result of a "launch Scout"
+			card, look at the top 2 cards instead of abd in the order of your choosing.
+			place each card on the top or the bottom of the deck.
+		 
+		No One Gets Left Behind:
+			Once per game, durring the Remove Ships step of Jumping the Fleet, lose
+			1 fuel to gain 2 population..
+		 
+		Family Commitments:
+			If you are not on Galactica, draw 1 fewer Skill Card during your
+			Receive Skills Step.
+		* /
+    },*/
+    AGATHON: {
+        name: 'Karl "Helo" Agathon',
+        type: CharacterTypeEnum.MILITARY_LEADER,
+        skills: {
+            leadership: 2,
+            tactics: 2,
+            piloting: 1,
+        },
+        start: LocationEnum.HANGAR_DECK,//see "Stranded"
+		/*
+		ECO Officer:
+		 	During your turn, you may reroll a die that was just rolled (once per turn).
+		 	You must use the new result.
+	 
+		Moral Compass:
+		 	Once per game, after a player makes a choice on a Crisis Card, you may change it.
+	 
+		Stranded:
+		 	Your character is not placed on the game board at the start of the game. While
+		 	not on the game board, you may not move, be moved, or take actions. At the start
+		 	of your second turn, place your character on the "Hanger Deck" location.
+		*/
+    },
+	ROSLIN:{
+        name:'Laura Roslin',
+        type:CharacterTypeEnum.POLITICAL_LEADER,
+        skills:{
+            politics:3,
+            leadership:2,
+        },
+        start: LocationEnum.PRESIDENTS_OFFICE,
+		/*
+		Religious Visions:
+			When you draw Crisis Cards, draw 2 and choose 1 to resolve. Place the
+			other on the bottom of the deck.
+		 
+		Skilled Politician - Action:
+			Once per game, draw 4 Quorum Cards. Chose 1 to resolve and place the
+			rest on the bottom of the deck. You do not need to be President to
+			use this ability.
+		 
+		Terminal Illness:
+			In order to activate a location, you must first discard 2 Skill Cards.
+		*/
+    },
+	VALERII:{
+        name:'Sharon "Boomer" Valerii',
+        type:CharacterTypeEnum.PILOT,
+        skills:{
+            tactics:2,
+            piloting:2,
+			engineering:1,
+        },
+        start: LocationEnum.ARMORY,
+		/*
+		Recon:
+			At the end of your turn, you may look at the top card of the Crisis
+			Deck and place it on the bottom.
+		 
+		Mysterious Intuition:
+			Once per game, before resolving a skill check on a Crisis Card, choose
+			the result (Pass or Fail), instead of resolving it normally.
+		 
+		Sleeper Agent:
+			During the Sleeper Agent Phase, you are dealt 2 Loyalty Cards
+			(instead of 1) and then moved to the "brig" location.
+		 */
+    },
+	TIGH:{
+        name:'Saul Tigh',
+        type:CharacterTypeEnum.MILITARY_LEADER,
+        skills:{
+            leadership:2,
+			tactics:3,
+        },
+        start: LocationEnum.COMMAND,
+		/*
+		Cylon Hatred:
+		 	When a player activates the "Admiral's  Quarters" location, you may
+		 	choose to reduce the difficulty by 3.
+		 
+		Declare Martial Law - Action:
+		 	Once per game, give the President title to the Admiral.
+		 
+		Alcoholic:
+			At the start of any player's turn, if you have exactly 1 Skill
+			Card in your hand, you must discard it.
+		*/
+    },
+	ZAREK:{
+        name:'Tom Zarek',
+        type:CharacterTypeEnum.POLITICAL_LEADER,
+        skills:{
+            politics:2,
+            leadership:2,
+            tactics:1,
+        },
+        start: LocationEnum.ADMINISTRATION,
+		/*
+		Friends in Low Places:
+		 	When a player activates the "Administration" or the "Brig" location,
+		 	you may choose to reduce or increase the difficulty by 2.
+		 
+		Unconventional Tactics - Action:
+		 	Once per game, lose 1 population to gain 1 of any other resource type.
+		 
+		Convicted Criminal:
+		 	You may not activate locations occupied by other characters (except the "brig").
+		*/
+    },
 });
 
 const SpaceEnum = Object.freeze({
