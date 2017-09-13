9d8e98e8815937842eb41dd01fd59bf9bf46c360
Orion Wolf_Hubbard <owolfhu1@gmail.com>
2017-09-13 04:59:18 -0400

    exodus Titles

--- exodus.js
+++ exodus.js
@@ -6,10 +6,12 @@
         - 7 Crossroads Cards - CrossroadMap
         - 3 Quorum Cards - QuorumMap
         - 3 Super Crisis Cards - SuperCrisisMap
-        -
+        - 2 alt Admiral & CAG roll cards - RollMap
 */
 
 const enums = require(__dirname + '/enums').enums;
+
+
 
 const CrisisMap = Object.freeze({});
 
@@ -25,6 +27,28 @@ const SuperCrisisMap = Object.freeze({});
 
 const LocationMap = Object.freeze({});
 
+const TitleMap = Object.freeze({
+
+    ADMIRAL : {
+        name : 'Admiral',
+        graphic : 'BSE_Title_Admiral.png',
+        text : 'When the fleet jumps, you draw 2 Destination Cards and choose 1. You control the nuke tokens.' +
+        'Action: Launch 1 nuke at a space area(the nuke token is discarded). 1-2 = Damage a basestar twice,' +
+        ' 3 - 6 = Destroy a basestar, 7 = Destroy a basestar and 3 raiders, 8 = Destroy every ship in the space area.'
+    },
+    
+    CAG : {
+        name : 'CAG',
+        graphic : 'BSE_Title_CAG.png',
+        text : 'When a civilian ship needs to be placed on the main game board, you choose the space area. ' +
+        'You must choose an area that does not already contain civilian ships (if able). ' +
+        'Action: Activate 1 unmanned viper and then give the CAG Title Card to any other human player. ' +
+        'Action:  Once per turn, if you are piloting a viper, you may activate 1 unmanned viper ' +
+        'and then take another action.',
+    },
+
+});
+
 exports.data = Object.freeze({
     CrisisMap : CrisisMap,
     AllyMap : AllyMap,
@@ -33,4 +57,5 @@ exports.data = Object.freeze({
     QuorumMap : QuorumMap,
     SuperCrisisMap : SuperCrisisMap,
     LocationMap : LocationMap,
+    TitleMap : TitleMap,
 });
